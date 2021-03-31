3a946834da826359120b3d13679c00cc2477c77a
IshraqQureshi <ishraqqureshi99@gmail.com>
2021-03-31 16:50:36 +0500

    Recursion example

---
+++ recursive.php
@@ -0,0 +1,19 @@
+<?php
+
+//Recursive functions
+
+function factorial($num){
+  if($num == 0){
+    return 0;
+  }
+  else if($num == 1){
+    return 1;
+  }
+  else{
+    return $num * factorial($num - 1);
+  }
+}
+
+echo factorial(5);
+
+?>
